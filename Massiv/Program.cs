a5222e2eedad2005cce21b4643d67962bf311d97
arizonskiyboec <arizonskiyboec@mail.ru>
2022-02-16 14:58:34 +0300

    Добавьте файлы проекта.

---
+++ Massiv/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Sortirovka()
+            {
+                int[] mass = new int[7];
+                int max=0;
+                int min=100; 
+                Random random = new Random();
+                Console.WriteLine("Начальный массив: ");
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i]=random.Next(1,100);
+                    if (mass[i] > max)
+                    {
+                        max=mass[i];
+                    }
+                    if (mass[i] < min)
+                    {
+                        min = mass[i];
+                    }
+                }
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    Console.Write(mass[i] + " ");
+                }
+                Console.WriteLine("\nОтсортированный массив");
+                int a;
+                for (int i = 0;i<mass.Length-1;i++)
+                {
+                    for (int j = i+1;j<mass.Length;j++)
+                    {
+                        if (mass[i]>mass[j])
+                        {
+                            a=mass[i];
+                            mass[i]=mass[j];
+                            mass[j]=a;
+                        }
+                    }
+                }
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    Console.Write(mass[i] + " ");
+                }
+                Console.WriteLine("\nMax "+max+"\nMin "+min);
+
+                Console.ReadKey();
+            }
+            Sortirovka();
+
+         
+        }
+    }
+}
---
+++ Solution/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваш вес: ");
+            double m = double.Parse(Console.ReadLine());
+            Console.Write("Введите ваш рост: ");
+            double h = double.Parse(Console.ReadLine());
+            h = h / 100;
+            Program imt = new Program();
+            Console.WriteLine("Индекс массы тела равен: " + imt.VIMT(m, h));
+            Console.ReadKey();
+
+        }
+        public double VIMT(double m,double h)
+        {
+            return m / (h * h);
+        }
+    }
+}
+
+
