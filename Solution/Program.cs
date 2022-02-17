52c007920d5986785cab1409cd2fc58c3798f4d4
arizonskiyboec <arizonskiyboec@mail.ru>
2022-02-17 12:04:34 +0300

    Add static void VivIMT

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Write("Введите ваш вес: ");
             double m = double.Parse(Console.ReadLine());
@@ -17,12 +17,51 @@ namespace Solution
             h = h / 100;
             Program imt = new Program();
             Console.WriteLine("Индекс массы тела равен: " + imt.VIMT(m, h));
-            Console.ReadKey();
-
+            double a=imt.VIMT(m, h);
+            VivIMT(ref a);
         }
         public double VIMT(double m,double h)
         {
             return m / (h * h);
+        }
+
+        static void VivIMT(ref double a)
+        {
+            if (a < 16)
+            {
+                Console.WriteLine("У вас значительный дефицит массы тела");
+                Console.ReadKey();
+            }
+            else if (a > 16 && a <= 18.5)
+            {
+                Console.WriteLine("У вас дефицит массы тела");
+                Console.ReadKey();
+            }
+            else if (a >= 18.5 && a <= 25)
+            {
+                Console.WriteLine("У вас нормальная масса тела");
+                Console.ReadKey();
+            }
+            else if (a >= 25 && a <= 30)
+            {
+                Console.WriteLine("У вас есть лишний вес");
+                Console.ReadKey();
+            }
+            else if (a >= 30 && a <= 35)
+            {
+                Console.WriteLine("У вас ожирение первой степени");
+                Console.ReadKey();
+            }
+            else if (a >= 35 && a <= 40)
+            {
+                Console.WriteLine("У вас ожирение второй степени");
+                Console.ReadKey();
+            }
+            else if (a >= 40)
+            {
+                Console.WriteLine("У вас ожирение третьей степени");
+                Console.ReadKey();
+            }
         }
     }
 }
