86076a542539dbb947dc51d967bdb0df6414e21c
ArizonskiyBoec <99715365+ArizonskiyBoec@users.noreply.github.com>
2022-03-13 20:50:03 +0300

    Add files via upload

---
+++ Dz2/WordCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordsAvg();
+        }
+        private static void WordsAvg()
+        {
+            var stringUser = GetStringUser("Введите строку");
+            var slovoKolvo = new Word();
+            var getWords = slovoKolvo.GetWordsArray(stringUser);
+            var slovoAvg = slovoKolvo.NumWord(getWords);
+            Console.ReadKey();
+        }
+        public static string GetStringUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine().ToLower();
+        }
+    }
+}
