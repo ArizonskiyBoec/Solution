a69c7edece39d9e2301f6ad1a62cd2ef0643aa94
ArizonskiyBoec <99715365+ArizonskiyBoec@users.noreply.github.com>
2022-04-14 12:07:01 +0300

    Add files via upload

---
+++ DZ6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetRound();
+        }
+        public static void SetRound()
+        {
+            var x = ToDouble(GetValueFromUser("Введите x координату центра"));
+            var y = ToDouble(GetValueFromUser("Введите y координату центра"));
+            var rad = ToDouble(GetValueFromUser("Введите радиус"));
+            var round =new Round(rad, x, y);
+            RoundMove(round);
+        }
+
+        private static double ToDouble(string value)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+                throw new ArgumentException();
+            return double.Parse(value);
+        }
+
+        public static string GetValueFromUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        /// <summary>
+        /// Задает координаты смещения в класс
+        /// </summary>
+        /// <param name="round">готовый круг</param>
+        public static void RoundMove(Round round)
+        {
+            var moveX = ToDouble(GetValueFromUser("Введите x смещение"));
+            var moveY = ToDouble(GetValueFromUser("Введите y смещение"));
+            round.Move(moveX, moveY);
+        }
+    }
+}
---
+++ DZ6/Round.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    public class Round
+    {   
+        //Радиус
+        public double R;
+        //x центр
+        public double RoundDx;
+        //y центр
+        public double RoundDy;
+        /// <summary>
+        /// Задает круг с введенными данными
+        /// </summary>
+        /// <param name="rad">Радиус</param>
+        /// <param name="x">Координата центра x</param>
+        /// <param name="y">Координата центра y</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Round(double rad, double x, double y)
+        {
+            this.RoundDx = x;
+            this.RoundDy = y;
+            if (rad <= 0)
+                throw new ArgumentNullException(nameof(rad), "Радиус меньше или равен нулю");
+            else
+                this.R = rad;
+        }
+        /// <summary>
+        /// Нахождение длины описаной окружности
+        /// </summary>
+        /// <returns>Длина</returns>
+        public double GetLenght()
+        {
+            return 2 * Math.PI * R;
+        }
+        /// <summary>
+        /// Нахождение площади круга
+        /// </summary>
+        /// <returns>Площадь круга</returns>
+        public double GetS()
+        {
+            return Math.PI*Math.Pow(R, 2);
+        }
+        /// <summary>
+        /// Движение круга
+        /// </summary>
+        /// <param name="moveX">Смещение по х</param>
+        /// <param name="moveY">Смещение по у</param>
+        public void Move(double moveX,double moveY)
+        {
+            this.RoundDx=RoundDx+moveX;
+            this.RoundDy=RoundDy+moveY;
+        }
+
+    }
+}
