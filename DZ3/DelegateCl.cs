f0ce48822ce8ca1219cef9aa98f43b99036aacdf
ArizonskiyBoec <99715365+ArizonskiyBoec@users.noreply.github.com>
2022-04-14 12:05:23 +0300

    Add files via upload

---
+++ DZ3/DelegateCl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegation
+{
+    internal class DelegateCl
+    {
+
+        public delegate int DelegateArray<T>(T i, T j);
+
+        public EventHandler ArraySortFinish;
+
+        public void SortArray<T>(T[] a, DelegateArray<T> mass)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (mass(a[i], a[j]) < 0)
+                    {
+                        var array = a[i];
+                        a[i] = a[j];
+                        a[j] = array;
+                    }
+                }
+            }
+            foreach (var i in a)
+            {
+                Console.Write(" " + i);
+            }
+            if (ArraySortFinish != null)
+            {
+                ArraySortFinish.Invoke(this, null);
+            }
+        }
+    }
+}
---
+++ DZ3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arraySort = new DelegateCl();
+            arraySort.ArraySortFinish += SortFinish;
+            var arrayS = new int[] { 10, 20, 30, 40, -10, -20, 28, 0 };
+            arraySort.SortArray(arrayS, ArrayS);
+            Console.ReadKey();
+        }
+
+        public static int ArrayS(int i, int j)
+        {
+            return i + j;
+        }
+
+        static void SortFinish(object sender, EventArgs e)
+        {
+            Console.WriteLine("Сортировка закончена");
+        }
+    }
+}
