834a6423c5f015d1a3167daf183b0bce5838eb38
ArizonskiyBoec <99715365+ArizonskiyBoec@users.noreply.github.com>
2022-04-14 12:06:04 +0300

    Add files via upload

---
+++ DZ4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionGit
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Date { get; set; }
+    }
+}
---
+++ DZ4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SolutionGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car();
+        }
+
+        public static void Car()
+        {
+            var car = new Car
+            {
+                Name = "lada",
+                Model = "Kalina",
+                Date = 2007
+            };
+            string json = JsonConvert.SerializeObject(car);
+            var car1 = JsonConvert.DeserializeObject<Car>(json);
+        }
+    }
+}
