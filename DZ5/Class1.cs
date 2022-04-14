f6b2d45bf3dc4032482ec3d0bcd7dedf2b8f61dd
ArizonskiyBoec <99715365+ArizonskiyBoec@users.noreply.github.com>
2022-04-14 12:06:35 +0300

    Add files via upload

---
+++ DZ5/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    public class Collection
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public static void GetColl()
+        {
+            var coll = new List<int>();
+            for (var i = 0; i < 20;i++)
+            {
+                coll.Add(i);
+            }
+            //Доступ к коллекции через Фильтрацию
+            var sort = from item in coll
+                       where item < 10
+                       select item;
+            foreach(var item in sort)
+            {
+                Console.WriteLine(item);
+            }
+            //Коллекция через группировку
+            var coll1 = new Collection[]
+            {
+                new Collection {Name = "Tesla",Age=2020 },
+                new Collection {Name ="Tesla",Age=2021}
+            };
+            var group = coll1.GroupBy(a => a.Age > 2020);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZ5/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Class2
+    {
+        public static void GetNumbers()
+        {
+            var numbers = new List<int>();
+            for (var i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+            var array =numbers.ToArray();//int значения заносятся в массив
+            var list = array.ToList<int>();//значения массива переходят в int
+            var take = numbers.Take(5);//Выводит первые 5 элементов
+            var skip=numbers.Skip(5);//выводит элементы после пятого
+            var order=numbers.OrderByDescending(x => x);//Элементы по убыванию
+            var any=numbers.Any();//Проверка на пустой список
+            var first =numbers.First();//Первый элемент 
+        }
+    }
+}
